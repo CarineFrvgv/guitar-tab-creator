0377e7089a0f74f334a575a860c62602ae93674c
Carine <carinefrvga@gmail.com>
2023-03-30 17:40:02 -0300

    change section notes to be of NoteType

--- src/entities/barSection/BarSection.ts
+++ src/entities/barSection/BarSection.ts
@@ -2,41 +2,85 @@ import {Note, NoteType, NUMBER_OF_GUITAR_FRETS, NUMBER_OF_GUITAR_STRINGS} from '
 
 interface IBarSection{
   getBarSection(): NoteType[]
-  addNewNoteToSection(note:Note): void
-  removeNoteFromSection(note: Note): void|string
+  addNewNoteToSection(note:NoteType): void
+  removeNoteFromSection(note: NoteType): void|string
+}
+
+type stringHasNoteType = {
+  stringIsInSection: boolean
+  indexOfNote: number
 }
 
 export class BarSection implements IBarSection{
-  private notes
+  private notes: NoteType[]
+  
+  constructor(notes?: NoteType[]){
+    if (notes){
+      this.notes = notes
+    }
+  }
 
-  constructor(notes: Note[]){
-    this.notes = notes
+  private  stringIsInSection(string:number): stringHasNoteType{
+    for(let i=0; i<this.notes.length; i++){
+      // since string is a unique value in
+      // this.notes i can compare by string
+      if(this.notes[i].guitarString == string){
+        return {stringIsInSection: true, indexOfNote: i}
+      }
+    }
+
+    return {stringIsInSection: false, indexOfNote: -1}
   }
 
   public getBarSection():NoteType[]{
-    let newArray:NoteType[] = []
+    return this.notes
+    // let newArray:NoteType[] = []
 
-    console.log(this.notes)
-    this.notes.forEach((note)=>{
-      console.log(note.id)
-      newArray.push(note.getNote())
-    })
+    // // console.log(this.notes)
+    // this.notes.forEach((note)=>{
+    //   // console.log(note.id)
+    //   newArray.push(note)
+    // })
 
-    return newArray;
+    // return newArray;
   }
 
-  public addNewNoteToSection(note:Note): void {
+  public addNewNoteToSection(note:NoteType): void {
+    // check first if string is occupied
+    if(this.notes.length > 0){
+      const { stringIsInSection, indexOfNote } = this.stringIsInSection(note.guitarString)
+
+      if(stringIsInSection){
+        this.notes.splice(indexOfNote, 1) // remove old note in specific string from section
+        this.notes.push(note) // add new note to section
+        return
+      }
+      
+    }
+
+
+    // if (this.notes.length == NUMBER_OF_GUITAR_STRINGS){
+    //   throw error
+    // }
+
     this.notes.push(note)     
     return   
   }
 
-  public removeNoteFromSection(note:Note): void|string{
-    let i = this.notes.indexOf(note)
-     
-    if(i !== -1){
-      this.notes.splice(i, 1) 
-      return
+  public removeNoteFromSection(note:NoteType): void|string{
+    for(let i=0; i<this.notes.length; i++){
+      if(this.notes[i].guitarString == note.guitarString){
+        this.notes.splice(i, 1);
+        return
+
+      }
     }
+    // let i = this.notes.indexOf(note)
+     
+    // if(i !== -1){
+    //   this.notes.splice(i, 1) 
+    //   return i.toString()
+    // }
 
     return 'not found';
   }
--- test/section.spec.ts
+++ test/section.spec.ts
@@ -11,17 +11,11 @@ let note1 = new Note({guitarFret:1, guitarString:2})
 let note2 = new Note({guitarFret:5, guitarString:6})
 let note3 = new Note({guitarFret:2, guitarString:3, guitarTechnique:'b'})
 
-// import { nanoid } from 'nanoid';
-// jest.mock(nanoid)
-
-// const mnanoid = mocked(nanoid)
 
 describe('',()=>{
-  const input = [note1,note2,note3]
+  const input = [note1.getNote(),note2.getNote(),note3.getNote()]
 
-
-
-  it('shold return the notes',()=>{
+  it('shold return the section',()=>{
     const newSection = new BarSection(input)
 
     let output = [
@@ -36,7 +30,7 @@ describe('',()=>{
   it('should add a new note to barsection',()=>{
     const newSection = new BarSection(input)
 
-    let newNote = new Note({guitarFret:3, guitarString:5, guitarTechnique:'~'})
+    let newNote = {guitarFret:3, guitarString:5, guitarTechnique:'~'}
     newSection.addNewNoteToSection(newNote)
 
     let output = [
@@ -51,26 +45,52 @@ describe('',()=>{
 
 })
 
-
 describe('',()=>{
+  const input = [
+    {guitarFret:1, guitarString:2},
+    {guitarFret:5, guitarString:6},
+    {guitarFret:2, guitarString:3, guitarTechnique:'b'},
+  ]
 
-  it('should add a new note to barsection',()=>{
-    const input = [note1,note2,note3]
+  const newSection = new BarSection(input)
+
+  newSection.addNewNoteToSection({guitarFret:12, guitarString:6, guitarTechnique:'/'})
+    
+
+  it('should remove note if new note in the same string is added', ()=>{
+    expect(newSection.getBarSection()).toStrictEqual([
+      {guitarFret:1, guitarString:2},
+      {guitarFret:2, guitarString:3, guitarTechnique:'b'},
+      {guitarFret:12, guitarString:6, guitarTechnique:'/'},
+    ])
+  })
+})
+
+
+describe('remove method',()=>{
+
+  it('should remove a note from section',()=>{
+    const input = [
+            {guitarFret:1, guitarString:2},
+            {guitarFret:5, guitarString:6},
+            {guitarFret:2, guitarString:3, guitarTechnique:'b'},
+          ]
+
     const newSection = new BarSection(input)
 
-    newSection.removeNoteFromSection(note3)
+    newSection.removeNoteFromSection({guitarFret:2, guitarString:3, guitarTechnique:'b'})
     
     expect(newSection.getBarSection()).toEqual([{guitarFret:1, guitarString:2},
       {guitarFret:5, guitarString:6}])
   })
 
-  it('should not found',()=>{
-    const input = [note1,note2]
+  it('should return not found',()=>{
+    const input = [note1.getNote(),note2.getNote()]
     const newSection = new BarSection(input)
 
-    newSection.removeNoteFromSection(note3)
+    newSection.removeNoteFromSection(note3.getNote())
     
-    expect(newSection.removeNoteFromSection(note3)).toEqual('not found')
+    expect(newSection.removeNoteFromSection(note3.getNote())).toEqual('not found')
   })
 
   
